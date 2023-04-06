67cdc0c7d815473125a9a14dd2793153ac708ace
Davi Ribeiro <davi.rb@hotmail.com>
2023-04-06 16:08:43 -0300

    InnerHTML add

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 const form = document.getElementById("form-atividade");
+const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji Celebrando" />';
+const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji Triste" />';
+
+let linhas = '';
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
@@ -8,11 +13,15 @@ form.addEventListener('submit', function(e) {
     let linha = '<tr>';
     linha += `<td>${inputNomeAtividade.value}</td>`;
     linha += `<td>${inputNotaAtividade.value}</td>`;
-    linha += `<td>${inputNotaAtividade.value >=7 ? 'Aprovado' : 'Reprovado'}</td>`;
+    linha += `<td>${inputNotaAtividade.value >=7 ? imgAprovado : imgReprovado } </td>`;
     linha += `</tr>`;
 
-    const corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linha;
+    linhas += linha;
 
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+    inputNomeAtividade.value = '';
+    inputNotaAtividade.value = '';
 
 });
